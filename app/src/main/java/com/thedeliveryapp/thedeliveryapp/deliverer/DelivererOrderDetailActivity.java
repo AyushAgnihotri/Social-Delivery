e2898154c8dffaccc43b3142062d595a19da5cfb
Ayush Agnihotri <hsuyaagnihotri@gmail.com>
2018-06-07 19:33:40 +0530

    Added dependency and code for Show path but not working.Needs to be fixed.

--- app/src/main/java/com/thedeliveryapp/thedeliveryapp/deliverer/DelivererOrderDetailActivity.java
+++ app/src/main/java/com/thedeliveryapp/thedeliveryapp/deliverer/DelivererOrderDetailActivity.java
@@ -19,6 +19,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,6 +30,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.android.PolyUtil;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.TravelMode;
 import com.onesignal.OSPermissionSubscriptionState;
 import com.onesignal.OneSignal;
 import com.thedeliveryapp.thedeliveryapp.R;
@@ -36,10 +45,13 @@ import com.thedeliveryapp.thedeliveryapp.login.user_details.UserDetails;
 import com.thedeliveryapp.thedeliveryapp.user.UserOrderDetailActivity;
 import com.thedeliveryapp.thedeliveryapp.user.order.OrderData;
 
+import org.joda.time.DateTime;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.security.SecureRandom;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class DelivererOrderDetailActivity extends AppCompatActivity implements ConnectivityReceiver.ConnectivityReceiverListener {
 
@@ -278,6 +290,22 @@ public class DelivererOrderDetailActivity extends AppCompatActivity implements C
             }
         });
 
+        btn_show_path.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO implement show path
+                DateTime now = new DateTime();
+                DirectionsResult result =
+                        DirectionsApi.newRequest(getGeoContext())
+                                .mode(TravelMode.DRIVING).origin(origin)
+                                .destination(destination).departureTime(now)
+                                .await();
+
+
+            }
+        });
+
+
         btn_accept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -395,6 +423,39 @@ public class DelivererOrderDetailActivity extends AppCompatActivity implements C
     protected void onResume() {
         super.onResume();
         CheckConnectivityMain.getInstance().setConnectivityListener(DelivererOrderDetailActivity.this);
+    }
+
+
+    //Google Maps
+
+    private GeoApiContext getGeoContext() {
+        GeoApiContext geoApiContext = new GeoApiContext();
+        return geoApiContext.setQueryRateLimit(3)
+                .setApiKey(getString(R.string.directionsApiKey)).setConnectTimeout(1, TimeUnit.SECONDS)
+                .setReadTimeout(1, TimeUnit.SECONDS)
+                .setWriteTimeout(1, TimeUnit.SECONDS);
+    }
+
+    private void addMarkersToMap(DirectionsResult results,
+                                 GoogleMap mMap) {
+        mMap.addMarker(new MarkerOptions().position(new
+                LatLng(results.routes[0].legs[0].startLocation.lat,results.routes[0].legs[0].startLocation.lng)).title(results.routes[0].legs[0].startAddress));
+        mMap.addMarker(new MarkerOptions().position(new
+                LatLng(results.routes[0].legs[0].endLocation.lat,results.routes[0].legs[0].endLocation.lng)).title(results.routes[0].legs[
+                0].startAddress).snippet(getEndLocationTitle(results)));
+    }
+
+    private String getEndLocationTitle(DirectionsResult results){
+        return "Time :"+
+                results.routes[0].legs[0].duration.humanReadable + " Distance
+:" + results.routes[0].legs[0].distance.humanReadable;
+    }
+
+    private void addPolyline(DirectionsResult results, GoogleMap
+            mMap) {
+        List<LatLng> decodedPath =
+                PolyUtil.decode(results.routes[0].overviewPolyline.getEncodedPath());
+        mMap.addPolyline(new PolylineOptions().addAll(decodedPath));
     }
 
 }
